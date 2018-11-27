2bc2604254e5af347f227b1d6ea7dbd50228731c
tilak <tilak.poudel.314@gmail.com>
2018-11-27 20:14:44 +0545

    comit on main menu1

--- database/migrations/2018_11_27_134126_create_sub_menus_table.php
+++ database/migrations/2018_11_27_134126_create_sub_menus_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateSubMenusTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('sub_menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('main_menu_id');
+            $table->string('sub_menu_name');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('sub_menus');
     }
 }
